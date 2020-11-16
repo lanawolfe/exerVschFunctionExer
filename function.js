26e0811724094983de0ba15189e5b9c91fd8e7da
lanawolfe <lana.j.wolfe@utah.edu>
2020-11-16 15:21:10 -0700

    finished

---
+++ function.js
@@ -0,0 +1,18 @@
+function add (x, y) {
+    return x + y
+  }
+  add(10 + 20)
+  console.log(10 + 20);
+  
+  console.log(Math.max(4,5,1,3));
+  
+  const number = prompt("15: ");
+  
+  if(number % 2 == 0) {
+      console.log("The number is even.");
+  }
+  
+  else {
+      console.log("The number is odd.");
+  }
+  
